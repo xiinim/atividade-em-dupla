84609b5976c49f90b52e36efaca16ce738c894c4
Maria Eduarda <130998915+xiinim@users.noreply.github.com>
2023-05-21 17:05:12 -0300

    Create script3.js

---
+++ script3.js
@@ -0,0 +1,10 @@
+a = Number(window.prompt('Digite o primeiro valor'))
+b = Number(window.prompt('Digite o segundo valor'))
+if(a == b){
+    c = a + b
+    alert('o valor é: ' + c)
+}
+if(a != b){
+    c = a * b
+    alert('o valor é: ' + c)
+} 
