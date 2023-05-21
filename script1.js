83fd585c4dbc981fc24b52ed228d2623e3269ebb
Maria Eduarda <130998915+xiinim@users.noreply.github.com>
2023-05-21 16:56:20 -0300

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,7 @@
+a = Number(window.prompt('Digite o primeiro valor'))
+b = Number(window.prompt('Digite o segundo valor'))
+c = Number(window.prompt('Digite o terceiro valor'))
+
+var result = (a + b + c) / 3
+
+alert ('O resultado é: ' + result)
